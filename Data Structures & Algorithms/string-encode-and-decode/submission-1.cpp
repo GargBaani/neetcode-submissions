a5ab3032ddd7372ee85c45676306238733054c6a
Baani Garg <148209665+GargBaani@users.noreply.github.com>
2026-06-27 11:11:00 +0530

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+// here we are takin a delimiter # to separate out the one string from another and if # is also behaving as a part of original string then we need an another delimiter so we take length of string 
+
+
+    string encode(vector<string>& strs) {
+        string str;
+        for(string s:strs){
+            str.append(to_string(s.length()));
+            str.push_back('#');
+            str.append(s);
+        }
+        return str;
+    }
+
+    vector<string> decode(string s) {
+        vector<string>res;
+        int i=0;
+        while(i< s.length()){
+            int j=i;
+            while(s[j]!='#')j++;
+            // substr deals with starting index then kitni length tk not the ending idx
+            int len=stoi(s.substr(i,j-i));
+            i=j+1;
+            j=i+len;
+            res.push_back(s.substr(i,len));
+            i=j;
+        }
+        return res;
+    }
+};
