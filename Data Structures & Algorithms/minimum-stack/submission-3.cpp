607230e733b3c75bc90977ea8034f74b2a227cd0
Baani Garg <148209665+GargBaani@users.noreply.github.com>
2026-07-18 12:12:47 +0530

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.cpp
@@ -0,0 +1,45 @@
+class MinStack {
+public:
+    stack<long long>st;
+    long long mini;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(st.empty())
+        {st.push(val);
+        mini=val;
+         return;}
+        if( mini> val){
+            st.push(2LL*val-mini);
+            mini=val;
+           
+        }else{
+            st.push(val);
+        }
+    }
+    
+    void pop() {
+        if(st.empty())return;
+        long long  x=st.top();
+        st.pop();
+        if(x<mini){
+            mini=2LL*mini-x;
+        }
+    }
+    
+    int top() {
+        if(st.empty())return -1;
+        long long x=st.top();
+        if(x<mini){
+            return mini;
+        }
+        return x;
+    }
+    
+    int getMin() {
+        if(st.empty())return -1;
+        return mini;
+    }
+};
